db0982a6e7cc4c9095eb353843085e7ce427b42b
Jean-Francois Remy <jean-francois.remy@technicolor.com>
2015-02-26 11:11:32 +0100

    Update to latest version of mocha / should + fix tests. Also use my version of compound

--- test/mailer.test.js
+++ test/mailer.test.js
@@ -3,22 +3,20 @@ describe('CompoundMailer', function() {
     var compound = app.compound;
 
     it('should load mailers and config on boot', function(done) {
-        compound.on('mailers', function () {
-            compound.structure.mailers.should.have.ownProperty('user');
-            compound.should.have.ownProperty('mailer');
-            compound.mailer.config.driver.should.eql('Stub');
-            done();
-        });
+        compound.structure.mailers.should.have.ownProperty('user');
+        compound.should.have.ownProperty('mailer');
+        compound.mailer.config.driver.should.eql('Stub');
+        done();
     });
 
     it('should send email', function(done) {
         var user = {name: 'Ben Afflek'};
         compound.mailer.send('user/registration', user, function(err, res) {
             if (err) throw err;
-            res.message.should.include('Subject: Welcome!');
-            res.message.should.include('Hello <strong>Ben Afflek</strong>!');
-            res.message.should.include('Hello, Ben Afflek.');
-            res.message.should.include('HEADER');
+            res.message.indexOf('Subject: Welcome!').should.not.be.equal(-1);
+            res.message.indexOf('Hello <strong>Ben Afflek</strong>!').should.not.be.equal(-1);
+            res.message.indexOf('Hello, Ben Afflek.').should.not.be.equal(-1);
+            res.message.indexOf('HEADER').should.not.be.equal(-1);
             // console.log(res.message.split('\n\r?\n')[0]);
             // console.log(res.message.split('\n\r?\n')[1]);
             done();
@@ -28,9 +26,9 @@ describe('CompoundMailer', function() {
     it('should setup default layout', function(done) {
         compound.mailer.send('e-commerce/order', function(err, res) {
             if (err) throw err;
-            res.message.should.include('Header');
-            res.message.should.include('Footer');
-            res.message.should.include('<a href=3D=22/order=22>Your order</a>');
+            res.message.indexOf('Header').should.not.be.equal(-1);
+            res.message.indexOf('Footer').should.not.be.equal(-1);
+            res.message.indexOf('<a href=3D=22/order=22>Your order</a>').should.not.be.equal(-1);
             done();
         });
     });
@@ -52,7 +50,7 @@ describe('CompoundMailer', function() {
     it('should reconnect when connect called again', function() {
         var transport = compound.mailer.transport;
         compound.mailer.connect();
-        compound.mailer.transport.should.not.eql(transport);
+        compound.mailer.transport.should.not.equal(transport);
     });
 
 });
